b3297aeeaa34f03a1bfb55088fb50f7bd8066cad
Drzaln <drzaln@null.net>
2019-07-27 16:01:52 +0700

    JWT udah bisa lagi!!!!!!!

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,8 +4,8 @@ const Auth = require('../helpers/auth')
 
 const userController = require('../controllers/user')
 
-Route
-  .get('/', userController.getUser)
+Route.all('/*', Auth.authInfo)
+  .get('/', Auth.accessToken, userController.getUser)
   .post(`/register`, userController.register)
   .post(`/login`, userController.login)
   .patch(`/:id_user`, userController.updateUser)
